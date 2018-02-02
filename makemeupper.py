862111daf8ac150dd4ebb8dd15bd9c7d4496c27a
Shey <sheymouse@gmail.com>
2018-02-02 16:04:31 +0000

    Added the main script

---
+++ makemeupper.py
@@ -0,0 +1,18 @@
+
+print("")
+print("=============== MAKE STRINGS UPPERCASE ===============")
+print("")
+print("A small app to take a string entered by a user and ")
+print("output that string in uppercase")
+print("")
+print("======================================================")
+print("")
+print("")
+print("")
+
+
+a = input("What do you want capitalised: ")
+print("")
+print("")
+print(a.upper())
+
